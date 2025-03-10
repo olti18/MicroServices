7b7635f16d3a32cd7e44d0f96345a410d907c5a2
Olti Berisha <olti.berisha.150@gmail.com>
2025-03-10 23:04:43 +0100

    fixed some errors

--- accounts/src/main/java/com/oltiberisha/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/oltiberisha/accounts/AccountsApplication.java
@@ -1,14 +1,17 @@
 package com.oltiberisha.accounts;
 
+import com.oltiberisha.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @OpenAPIDefinition(
 		info=@Info(
 				title = "Accounts microservices REST API Documentation",
--- accounts/src/main/java/com/oltiberisha/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/oltiberisha/accounts/controller/AccountsController.java
@@ -1,17 +1,19 @@
 package com.oltiberisha.accounts.controller;
-
+import org.springframework.core.env.Environment;
 import com.oltiberisha.accounts.constans.AccountConstans;
-import com.oltiberisha.accounts.dto.CustomerDto;
-import com.oltiberisha.accounts.dto.ResponseDto;
+import com.oltiberisha.accounts.dto.*;
 import com.oltiberisha.accounts.service.IAccountService;
-import com.oltiberisha.accounts.dto.AccountsDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +25,24 @@ import org.springframework.web.bind.annotation.*;
         description = "CRUD REST APIs for Accounts to CREATE UPDATE DELETE and FETCH Account Details")
 @RestController
 @RequestMapping(path="/api",produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class AccountsController {
 
-    private IAccountService iAccountService;
+//    private IAccountService iAccountService;
+    private final IAccountService iAccountService;
+    public AccountsController(IAccountService iAccountService) {
+        this.iAccountService = iAccountService;
+    }
+    @Value("${build.version:1.0.0}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    //private AccountsContactInfoDto accountsContactInfoDto = new AccountsContactInfoDto(); // Instanca manuale
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     @Operation(
             summary = "Create Account REST API",
@@ -91,6 +106,40 @@ public class AccountsController {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ResponseDto(AccountConstans.STATUS_500, AccountConstans.MESSAGE_500));
         }
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+
+
+    @Operation(
+            summary = "Get Contact Info",
+            description = "Contact Info details that can be reached out in case of any issues"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto);
     }
 
 
